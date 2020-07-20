7c6ca66c87f6d5955cfdc8040ad7143b7bc48a60
Your Name <you@example.com>
2020-07-20 17:13:33 +0900

    list for stemp1

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask ,render_template
+from data import Articles
 
 app = Flask(__name__)
 app.debug=True
 
-@app.route('/data')
+@app.route('/')
 def index():
-    print("Success")
+    print("Suceesss")
     # return "TEST"
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    print("Suceesss")
+    # return "TEST"
+    return render_template('about.html')
+
+
+
+
+@app.route('/articles')
+def article():
+    
+    # return "TEST"
+    articles = Articles()
+    print(len(articles))
+    return render_template('articles.html',articles=articles)
 if __name__ =='__main__':
     # app.run(host='0.0.0.0', port='8080')
     app.run()
